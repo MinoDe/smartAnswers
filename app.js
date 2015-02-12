0c2a4716cf606d86744b3c39b38bc875c445345d
Mino De Raj <minoderaj@gmail.com>
2015-02-11 19:13:21 -0800

    status updates

---
+++ app.js
@@ -0,0 +1,164 @@
+var os = require('os');
+var http = require('http');
+var express = require('express');
+var connect = require("connect");
+var blessed = require('blessed');
+var bodyParser = require('body-parser');
+var querystring = require('querystring');
+var app = express();
+
+app.use(bodyParser.urlencoded());
+
+// Create a screen object.
+var screen = blessed.screen();
+
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+	top: 'center',
+	left: 'center',
+	width: '50%',
+	height: '50%',
+	content: '',
+	tags: true,
+	border: {
+		type: 'line'
+	},
+	style: {
+		fg: 'white',
+		bg: 'black',
+		border: {
+			fg: '#f0f0f0'
+		},
+		hover: {
+			bg: 'black'
+		}
+	}
+});
+
+// Append our box to the screen.
+screen.append(box);
+
+app.set('port', process.env.PORT || 3000);
+
+var my_group = ["192.168.0.101", "192.168.0.103", "192.168.1.104", "192.168.1.105"];	// replace with real IPs of group
+
+var my_index = 1;	// replace with index of my IP in my_group
+
+box.setContent('this node (' + my_group[my_index] + ') will attempt to send its token to other nodes on network. ');
+screen.render();
+
+// handle GET requests
+app.get('/do_get', function (req, res){
+	var the_body = req.query;
+	console.log ( "get body: " + the_body );
+	box.setContent("Get with query: " + the_body);
+	box.style.bg = 'green';	//green for get
+	screen.render();
+	res.json({"query": the_body, "id": JSON.stringify(my_group[my_index])});
+});
+
+
+// handle POST requests
+app.post('/do_post', function (req, res) {
+	var post_data = req.body;	//see connect package above
+	/*console.log ( "post body: " + the_body );
+	res.json({"body": the_body, "id": JSON.stringify(my_group[my_index])});*/
+
+	//box.setContent("Count is at: " + post_data.n + ", with " + post_data.c + " primes found.");
+	var count = countPrimes(post_data);
+	
+	postToNext({c:count,n:num,k:kwork});
+	}, 0);
+	//box.style.bg = "green";
+	//screen.render();
+});
+
+function countPrimes(post_data) {
+	var kwork = post_data.k;
+	var num = post_data.n;
+	var count = post_data.c;
+
+	box.setContent("Running computations for " + post_data.k + " milliseconds");
+	box.style.bg = "green";
+	screen.render();
+
+	setTimeout(function() {
+	// Get Primes
+         var prevTime = (new Date()).getTime();
+         var status = 1;
+
+         while(1)
+     {
+         var curTime = (new Date()).getTime();
+                 var deltaTime = curTime - prevTime;
+
+                 if(deltaTime > kwork)
+                        break;
+         var j = 2;
+         for (  j = 2 ; j <= Math.sqrt(num) ; j++ )
+         {
+            if ( num%j == 0 )
+            {
+               status = 0;
+               break;
+            }
+         }
+         if ( status != 0 )
+         {
+            count++;
+         }
+         status = 1;
+         num++;
+
+
+      }
+	return count;
+
+}
+
+function postToNext(data) {
+	box.style.bg = "yellow";
+	box.setContent("Numbers looked at: " + data.n + ". Primes found: " + data.c);
+	screen.render();
+
+	var post_data = querystring.stringify(data);
+	var post_options = {
+		host: my_group[(my_index + 1) % my_group.length],
+		port: '3000',
+		path: '/do_post',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': post_data.length
+		}
+	};
+	var post_req = http.request(post_options, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+
+		});
+	});
+
+	// post the data
+	post_req.write(post_data);
+	post_req.end();
+
+}
+if(my_index == 0) {
+	countPrimes({c:0,n:0,k:5000});
+}
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+	return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+
+// Render the screen.
+screen.render();
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
---
+++ discovery.js
@@ -0,0 +1,48 @@
+var Discover = require('node-discover');
+
+var d = Discover();
+
+d.on("promotion", function () {
+    /* 
+     * Launch things this master process should do.
+     * 
+     * For example:
+     *  - Monitior your redis servers and handle failover by issuing slaveof
+     *    commands then notify other node instances to use the new master
+     *  - Make sure there are a certain number of nodes in the cluster and 
+     *    launch new ones if there are not enough
+     *  - whatever
+     * 
+     */
+
+    console.log("I was promoted to a master.");
+});
+
+d.on("demotion", function () {
+    /*
+     * End all master specific functions or whatever you might like. 
+     *
+     */
+
+    console.log("I was demoted from being a master.");
+});
+
+d.on("added", function (obj) {
+    console.log("A new node has been added.");
+});
+
+d.on("removed", function (obj) {
+    console.log("A node has been removed.");
+});
+
+d.on("master", function (obj) {
+    /*
+     * A new master process has been selected
+     * 
+     * Things we might want to do:
+     *  - Review what the new master is advertising use its services
+     *  - Kill all connections to the old master
+     */
+
+    console.log("A new master is in control");
+});
